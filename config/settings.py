0a573b6b5201095df75f0f902a9d7a9813a67a79
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-18 21:38:55 -0700

    build(django): setup ORM

---
+++ config/settings.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ysce8fn=3@!@f()-z69+21+-d@1o=-9k_g-n+^bcwl4q&kyx5h"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+
+# Application definition
+
+INSTALLED_APPS = ["db"]
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / ".db/Chinook_Sqlite.sqlite",
+    }
+}
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
---
+++ crud.py
@@ -0,0 +1,10 @@
+# Import settings
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+# setup django environment
+import django
+
+django.setup()
+
