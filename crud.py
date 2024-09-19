a2520c543e30ec89e9a5de0ad58dbbe2a6fd63af
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-19 13:09:01 -0700

    feat(crud): GET albums

--- crud.py
+++ crud.py
@@ -8,7 +8,7 @@ import django
 
 django.setup()
 
-from db.models import Artist
+from db.models import Artist, Album
 
 
 def list_artists(limit: int, offset: int = 0):
@@ -22,3 +22,25 @@ def get_artist(id: int):
         return None
 
     return artist
+
+
+def list_albums(limit: int, offset: int = 0):
+    return list(Album.objects.values()[offset : offset + limit])
+
+
+def get_album(id: int):
+    try:
+        album = Album.objects.values().get(pk=id)
+    except Album.DoesNotExist:
+        return None
+
+    return album
+
+
+def get_albums_by_artist(id: int):
+    try:
+        artist = Artist.objects.get(pk=id)
+    except Artist.DoesNotExist:
+        return None
+
+    return list(artist.album_set.values())
