7343610db386925a1e8fdb1e97f290cb4f84333a
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-18 22:15:13 -0700

    feat(crud): GET artists

--- crud.py
+++ crud.py
@@ -8,3 +8,17 @@ import django
 
 django.setup()
 
+from db.models import Artist
+
+
+def list_artists(limit: int, offset: int = 0):
+    return list(Artist.objects.values()[offset : offset + limit])
+
+
+def get_artist(id: int):
+    try:
+        artist = Artist.objects.values().get(pk=id)
+    except Artist.DoesNotExist:
+        return None
+
+    return artist
