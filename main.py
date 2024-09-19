2227234b3a3f68f6c7df22c8fff486ce47d2a55f
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-19 12:56:13 -0700

    refactor(api): implement routers

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
 
-from crud import list_artists, get_artist
+from routers import artists
 
 app = FastAPI()
 
-
-class ArtistObject(BaseModel):
-    id: int = 0
-    name: str
-    type: str = "artist"
-
-
-class ArtistList(BaseModel):
-    limit: int
-    offset: int
-    items: list[ArtistObject]
-
-
-@app.get("/artists", tags=["Artists"])
-def artists(limit: int = 10, offset: int = 0) -> ArtistList:
-    result = list_artists(limit, offset)
-
-    return {"limit": limit, "offset": offset, "items": result}
-
-
-@app.get("/artists/{id}", tags=["Artists"])
-def artist(id: int) -> ArtistObject:
-    result = get_artist(id)
-
-    if not result:
-        raise HTTPException(status_code=404)
-
-    return result
+app.include_router(artists.router, prefix="/artists")
---
+++ routers/artists.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
+
+from crud import list_artists, get_artist, get_albums_by_artist
+from .albums import AlbumObject
+
+
+class ArtistObject(BaseModel):
+    id: int = 0
+    name: str
+    type: str = "artist"
+
+
+class ArtistList(BaseModel):
+    limit: int
+    offset: int
+    items: list[ArtistObject]
+
+
+router = APIRouter(tags=["artists"])
+
+
+@router.get("/")
+def artists(limit: int = 10, offset: int = 0) -> ArtistList:
+    result = list_artists(limit, offset)
+
+    return {"limit": limit, "offset": offset, "items": result}
+
+
+@router.get("/{id}")
+def artist(id: int) -> ArtistObject:
+    result = get_artist(id)
+
+    if not result:
+        raise HTTPException(status_code=404)
+
+    return result
