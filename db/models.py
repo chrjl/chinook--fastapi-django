e8c8e3c3593b7e78dffc6c955024d605978a5966
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-19 13:08:42 -0700

    feat(models): Album

--- db/models.py
+++ db/models.py
@@ -7,3 +7,14 @@ class Artist(models.Model):
 
     class Meta:
         db_table = "Artist"
+
+
+class Album(models.Model):
+    id = models.IntegerField(db_column="AlbumId", primary_key=True)
+    title = models.CharField(db_column="Title", max_length=160)
+    artist = models.ForeignKey(
+        Artist, db_column="ArtistId", on_delete=models.DO_NOTHING
+    )
+
+    class Meta:
+        db_table = "Album"
