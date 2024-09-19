f56f234776466bcfae61509c020c5d6a89b377ae
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-18 22:03:09 -0700

    feat(models): Artist

--- db/models.py
+++ db/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+
+class Artist(models.Model):
+    id = models.IntegerField(db_column="ArtistId", primary_key=True)
+    name = models.CharField(db_column="Name", max_length=120)
+
+    class Meta:
+        db_table = "Artist"
