6803104ccd704e11adb348cd44ea0d24b5970afb
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-22 14:12:08 -0700

    feat(FE): Root route - Navbar, scaffold routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,30 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Root from './routes/root';
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello World!</div>,
+    element: <Root />,
+    children: [
+      {
+        path: '/artists',
+        element: <div>Artists</div>,
+      },
+      {
+        path: '/albums',
+        element: <div>Albums</div>,
+      },
+      {
+        path: '/tracks',
+        element: <div>Tracks</div>,
+      },
+      {
+        path: '/genres',
+        element: <div>Genres</div>,
+      },
+    ],
   },
 ]);
 
---
+++ frontend/src/routes/root.tsx
@@ -0,0 +1,50 @@
+import { Outlet, Link } from 'react-router-dom';
+
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import githubMark from '../assets/github-mark-white.svg';
+
+export default function Root() {
+  return (
+    <>
+      <Navigation />
+      <Container>
+        <Outlet />
+      </Container>
+    </>
+  );
+}
+
+export function Navigation() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand>chinook</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link to="/artists" as={Link}>
+            Artists
+          </Nav.Link>
+          <Nav.Link to="/albums" as={Link}>
+            Albums
+          </Nav.Link>
+          <Nav.Link to="/tracks" as={Link}>
+            Tracks
+          </Nav.Link>
+          <Nav.Link to="/genres" as={Link}>
+            Genres
+          </Nav.Link>
+        </Nav>
+        <Navbar.Text>
+          <a
+            href="https://github.com/chrjl/chinook--fastapi-django"
+            target="_blank"
+          >
+            <img height="24px" src={githubMark} />
+          </a>
+        </Navbar.Text>
+      </Container>
+    </Navbar>
+  );
+}
