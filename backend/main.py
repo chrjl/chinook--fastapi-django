5f03bf83ef445ac219898d03f44ca77029f57acd
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-22 14:10:15 -0700

    refactor(api): add root_path

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from .routers import artists, albums, genres, tracks
 
-app = FastAPI()
+app = FastAPI(root_path="/api")
 
 app.include_router(artists.router, prefix="/artists")
 app.include_router(albums.router, prefix="/albums")
