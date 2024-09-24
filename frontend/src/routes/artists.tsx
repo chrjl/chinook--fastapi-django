38bf24d81faa4d3011731de82a8ada0fccb13071
chrjl <73675826+chrjl@users.noreply.github.com>
2024-09-24 09:59:48 -0700

    refactor(FE/artists): implement Modal

--- frontend/src/routes/artists.tsx
+++ frontend/src/routes/artists.tsx
@@ -1,22 +1,25 @@
 import { useState, useEffect } from 'react';
-import { Link, useOutletContext } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 import SimplePagination from '../utilities/simple-pagination';
 
-interface ArtistObject {
+export interface ArtistObject {
   id: number;
   name: string;
   type: 'artist';
 }
 
 export default function Artist() {
-  const [artists, setArtists] = useState<ArtistObject[]>([]);
+  const [artistList, setArtistList] = useState<ArtistObject[]>([]);
   const [page, setPage] = useState<number>(1);
   const [total, setTotal] = useState<number>(0);
-  const [selected, setSelected] = useOutletContext();
+  const [selectedArtistId, setSelectedArtistId] = useState<number | null>(null);
+  const [showModal, setShowModal] = useState(false);
 
   const limit = 20;
   const totalPages = Math.ceil(total / limit);
@@ -30,27 +33,82 @@ export default function Artist() {
   useEffect(() => {
     fetch(`/api/artists?limit=${limit}&offset=${(page - 1) * limit}`)
       .then((res) => res.json())
-      .then((res) => setArtists(res.items));
+      .then((res) => setArtistList(res.items));
   }, [page]);
 
   return (
-    <div>
+    <>
+      <h1>Artists</h1>
+
+      {selectedArtistId && (
+        <ArtistModal
+          id={selectedArtistId}
+          show={showModal}
+          setShow={setShowModal}
+        />
+      )}
+
       <Container className="d-flex flex-column align-items-center">
         <SimplePagination setPage={setPage} first={1} last={totalPages} />
       </Container>
 
-      <ListGroup>
-        {artists.map(({ id, name }) => (
-          <ListGroup.Item key={id}>
-            <Link
-              to="/albums"
-              onClick={() => setSelected({ artist: { id, name } })}
-            >
-              {name}
-            </Link>
+      <ListGroup variant="flush">
+        {artistList.map(({ id, name }) => (
+          <ListGroup.Item
+            key={id}
+            action
+            onClick={() => handleSelectArtist(id)}
+          >
+            {name}
           </ListGroup.Item>
         ))}
       </ListGroup>
-    </div>
+    </>
   );
+
+  function handleSelectArtist(id: number) {
+    setSelectedArtistId(id);
+    setShowModal(true);
+  }
+}
+
+interface ArtistModalProps {
+  id: number;
+  show: boolean;
+  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function ArtistModal({ id, show, setShow }: ArtistModalProps) {
+  const [artist, setArtist] = useState<ArtistObject | null>(null);
+
+  useEffect(() => {
+    fetch(`/api/artists/${id}`)
+      .then((res) => res.json())
+      .then((res) => setArtist(res));
+  }, [id]);
+
+  return (
+    artist && (
+      <Modal centered show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{artist.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>ArtistId: {artist.id}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Link to={`/artists/${artist.id}/albums`}>
+            <Button variant="primary">Browse albums</Button>
+          </Link>
+        </Modal.Footer>
+      </Modal>
+    )
+  );
+
+  function handleClose() {
+    setShow(false);
+  }
 }
